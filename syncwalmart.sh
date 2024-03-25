0e55c4df7780254ae331544a7095e669ce11b863
Rahul Madhavan <rahul.maddy@gmail.com>
2024-03-25 14:59:54 +0530

    content changes

--- syncwalmart.sh
+++ syncwalmart.sh
@@ -6,5 +6,7 @@ git pull origin main
 cp index.html /var/www/cfe-walmart/index.html
 cp people.html /var/www/cfe-walmart/people.html
 cp events.html /var/www/cfe-walmart/events.html
+cp news.html /var/www/cfe-walmart/news.html
+cp joinus.html /var/www/cfe-walmart/joinus.html
 cp -R assets /var/www/cfe-walmart/
 cp -R content /var/www/cfe-walmart/
