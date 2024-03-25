bf141840cc0ec41fdb70cb3c0b370581aec02cc2
Rahul Madhavan <rahul.maddy@gmail.com>
2024-03-25 09:43:07 +0530

    content changes

--- synchronizeCloud.sh
+++ synchronizeCloud.sh
@@ -1,20 +1,11 @@
-# Type the command "cmd < synchronizeCloudLinux.sh" on windows
-# Or the command "./synchronizeCloudLinux.sh" on linux
+# Type the command "cmd < synchronizeCloud.sh" on windows
+# Or the command "./synchronizeCloud.sh" on linux after ensuring chmod +rwx permissions on this file.
 # Preliminary Checks:
 # 1. Ensure "gcloud auth login" is already done
 # 2. Ensure that you are on the IISc CISCO VPN
 
-gsutil -m rsync -r assets gs://www.cstheoryseminars.org/assets
-echo "Assets Directory Synced"
-gsutil -m rsync -r content gs://www.cstheoryseminars.org/content
-echo "Contents Directory Synced"
-gsutil -m rsync ./ gs://www.cstheoryseminars.org
-# gsutil cp index.html gs://www.cstheoryseminars.org
-echo "Base Directory Synced"
-
-
-# Now Git changes
-git add -A
+# Git changes
+git add .
 git commit -m "content changes"
 git push
 
---
+++ syncwalmart.sh
@@ -0,0 +1,10 @@
+# This is the file on remote iisc host server that runs the synchronization commands.
+
+# In this file we will setup the sync
+cd cfe-walmart
+git pull origin main
+cp index.html /var/www/cfe-walmart/index.html
+cp people.html /var/www/cfe-walmart/people.html
+cp events.html /var/www/cfe-walmart/events.html
+cp -R assets /var/www/cfe-walmart/
+cp -R content /var/www/cfe-walmart/
