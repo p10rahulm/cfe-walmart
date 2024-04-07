fec7a0da3d228368665ad8896e1d093fbc8b1443
Rahul Madhavan <rahul.maddy@gmail.com>
2024-04-07 19:18:52 +0530

    dropdown implemented in all pages

--- syncwalmart.sh
+++ syncwalmart.sh
@@ -4,7 +4,10 @@
 cd cfe-walmart
 git pull origin main
 cp index.html /var/www/cfe-walmart/index.html
-cp people.html /var/www/cfe-walmart/people.html
+cp faculty.html /var/www/cfe-walmart/faculty.html
+cp postdocs.html /var/www/cfe-walmart/postdocs.html
+cp phds.html /var/www/cfe-walmart/phds.html
+cp predocs.html /var/www/cfe-walmart/predocs.html
 cp events.html /var/www/cfe-walmart/events.html
 cp news.html /var/www/cfe-walmart/news.html
 cp joinus.html /var/www/cfe-walmart/joinus.html
